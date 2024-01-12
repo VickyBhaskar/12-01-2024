919c323e4df058dd93b64279dcb33f6aeabb84ba
VickyBhaskar <vicky2400kumar@gmail.com>
2024-01-12 16:04:10 +0530

    initial Commit

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiDocker.Models;
+
+namespace WebApiDocker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private static List<Movies> tasks = new List<Movies>();
+
+        // GET: api/tasks
+        [HttpGet]
+        public IEnumerable<Movies> Get()
+        {
+            return tasks;
+        }
+
+        // GET: api/tasks/1
+        [HttpGet("{id}")]
+        public Movies Get(int id)
+        {
+            return tasks.Find(task => task.MId == id);
+        }
+
+        // POST: api/tasks
+        [HttpPost]
+        public void Post([FromBody] Movies task)
+        {
+            tasks.Add(task);
+        }
+
+        // PUT: api/tasks/1
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Movies updatedTask)
+        {
+            var index = tasks.FindIndex(task => task.MId == id);
+            if (index != -1)
+            {
+                tasks[index] = updatedTask;
+            }
+        }
+
+        // DELETE: api/tasks/1
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            tasks.RemoveAll(task => task.MId == id);
+        }
+    }
+
+}
---
+++ Models/Movies.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiDocker.Models
+{
+    public class Movies
+    {
+        public int MId { get; set; }
+        public string MName { get; set; }
+        public int Rank { get; set; }
+
+    }
+}
